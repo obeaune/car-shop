a5cee58cc313ef8be7afa707f7b47c822abc9a3a
obeaune <linecora86@gmail.com>
2022-08-16 20:28:04 -0300

    feat: create the `IService` interface

---
+++ src/interfaces/IService.ts
@@ -0,0 +1,7 @@
+export interface IService<T> {
+  create(obj:T):Promise<T>,
+  read(): Promise<T[]>,
+  readOne(arg0: string): Promise<T | null>,
+  update(arg0: string, obj: T): Promise<T | null>,
+  delete(arg0: string): Promise<T | null>,
+}
