460da9e2937779fd624c36e67bcf67f6b91c4c56
obeaune <linecora86@gmail.com>
2022-08-16 20:30:33 -0300

    feat: create `index` and fix `app`

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
 import express from 'express';
+import 'express-async-errors';
+import errorHandler from './middlewares/error';
+import CarRouter from './routes/carRouter';
 
 const app = express();
+
+app.use(express.json());
+app.use(CarRouter);
+app.use(errorHandler);
 
 export default app;
