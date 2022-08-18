f77e0d5531771baa0ee2c593bf3b2c222846f55d
obeaune <linecora86@gmail.com>
2022-08-18 17:10:05 -0300

    refactor: `app`

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import 'express-async-errors';
 import errorHandler from './middlewares/error';
-import CarRouter from './routes/carRouter';
+import indexRouter from './routes';
 
 const app = express();
 
 app.use(express.json());
-app.use(CarRouter);
+app.use(indexRouter);
 app.use(errorHandler);
 
 export default app;
